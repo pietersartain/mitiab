21c63d142b85c9ab7635580d8f72bfd610cdc2d1
Meredydd Luff <meredydd@senatehouse.org>
2016-05-15 11:41:17 +0100

    Fix startup script

--- support/startup-on-py.sh
+++ support/startup-on-py.sh
@@ -3,6 +3,9 @@
 ## This is the startup that happens AFTER boot, when the user has selected either to continue
 ## with a virgin database or to restore a backup from the USB drive
 
+cd "`dirname $0`"/ohc/outbreak
+
+. ../.env/bin/activate
 python manage.py migrate
 python manage.py collectstatic --noinput
 supervisord -c etc/pi.conf
