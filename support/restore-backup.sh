4ce7a45b84b11ca32b1ae1fd7b78305f24ff2d6c
Meredydd Luff <meredydd@senatehouse.org>
2016-05-15 14:17:46 +0100

    Small fix to restore script

--- support/restore-backup.sh
+++ support/restore-backup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Reloading from a backup
+set -e
 
 if [ "$1" == "" ]; then
 	echo "Usage: $0 <backup.sql>"
 	exit 1
 fi
 
-su postgres -c 'psql -c "DROP DATABASE outbreak;"'
+su postgres -c 'psql -c "DROP DATABASE outbreak;"' || true
 su postgres -c 'psql -c "CREATE DATABASE outbreak WITH OWNER outbreak;"'
 su postgres -c 'psql outbreak' < "$1"
 
